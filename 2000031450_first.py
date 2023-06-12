4cd944a17d42a499c631a39e7bd89e9f415b7591
PerniVamsi <96769343+PerniVamsi@users.noreply.github.com>
2023-06-12 11:34:37 +0530

    Update 2000031450_first.py

--- 2000031450_first.py
+++ 2000031450_first.py
@@ -1,15 +1,38 @@
+import requests
+from flask import Flask, request, jsonify
+from concurrent.futures import ThreadPoolExecutor
+import time
 
-def __init__(self, port):
-    self.port = port
+app = Flask(__name__)
 
-
-def get_numbers(self, urls):
-    numbers = []
-    for url in urls:
-        response = requests.get(url)
+def fetch_numbers(url):
+    try:
+        response = requests.get(url, timeout=0.5)
         if response.status_code == 200:
-            numbers.extend(json.loads(response.content)["numbers"])
+            data = response.json()
+            return data.get("numbers", [])
+    except (requests.RequestException, ValueError):
+        pass
+    return []
 
-    numbers = sorted(set(numbers))
-    return numbers
+@app.route('/numbers', methods=['GET'])
+def get_numbers():
+    urls = request.args.getlist('url')
 
+    numbers = []
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(fetch_numbers, url) for url in urls]
+
+        for future in futures:
+            try:
+                result = future.result()
+                numbers.extend(result)
+            except requests.Timeout:
+                pass
+
+    numbers = sorted(list(set(numbers)))
+
+    return jsonify({"numbers": numbers})
+
+if __name__ == '__main__':
+    app.run(port=8008)
